dc7f92c6a7d2aed2ff6e18bb63c6f58f6cfa6c30
surjendu104 <cyber00daddy@gmail.com>
2023-01-01 19:37:44 +0530

    fix

--- index.js
+++ index.js
@@ -25,12 +25,6 @@ app.use(function (req, res, next) {
     next();
 });
 
-// router.use((req, res, next) => {
-//     res.header("Access-Control-Allow-Origin", "*");
-//     res.header("Access-Control-Allow-Methods", " GET, PUT, POST, DELETE");
-//     next(); 
-// });
-
 
 const port = process.env.PORT || 8383
 
