4a598140a39e10b8e4a3dcb7ed14d6eababcbe08
surjendu104 <cyber00daddy@gmail.com>
2023-03-30 01:54:05 +0530

    feat : fixed the error in request handeling

--- index.js
+++ index.js
@@ -37,6 +37,8 @@ app.get('/',(req,res)=>{
     res.status(200).send("Hello world")
 })
 
+
+//Don't use this one . It is errorneous
 app.get('/getOutput', (req, res) => {
     var postData = qs.stringify({
         code: _code,
@@ -81,7 +83,9 @@ app.get('/getOutput', (req, res) => {
         });
 })
 
+// This will serve all requests
 app.post('/', (req, res) => {
+    // Forme the data for further use from the request from user
     const { code } = req.body
     const { language } = req.body
     const { input } = req.body
@@ -99,13 +103,55 @@ app.post('/', (req, res) => {
             c:_input
         })
         console.log("post : ",dmd)
-        res.status(200).send(dmd)
+        // res.status(200).send(dmd)
     }
     else{
         console.log("erroe")
-        res.send("error")
+        // res.send("error")
     }
+
+    // Post The data
     
+    var postData = qs.stringify({
+        code: _code,
+        language: _lang,
+        input: _input
+    })
+    console.log("data : ", postData)
+    var config = {
+        method: "post",
+        url: "https://api.codex.jaagrav.in",
+        headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+        },
+        data: postData,
+    }
+
+    axios(config)
+        .then(function (res) {
+            // console.log(res.data)
+           _output = JSON.stringify(res.data)
+           res.data={}
+           return _output
+        })
+        .then(()=>{
+
+            console.log("output : ",_output)
+            if((JSON.parse(_output)).status==200){
+                if((JSON.parse(_output)).error==""){
+                    res.status(200).send({ getOutput: (JSON.parse(_output)).output})
+                }
+                else {
+                    res.status(200).send({ getOutput: (JSON.parse(_output)).error})
+                }
+            }
+            else {
+                res.send({ getOutput: (JSON.parse(_output)).error})
+            }
+        })
+        .catch((error)=> {
+            console.log(error);
+        });
 })
 
 app.listen(port, () => console.log(`Server has started on port ${port}`))
