419917f70299e1ec7c7dcf7e0f94501dc61e6336
surjendu104 <cyber00daddy@gmail.com>
2023-03-30 09:22:44 +0530

    feat : final refactorinf

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const cors = require("cors")
 const app = express()
 
 app.use(cors({
-    origin:"*",
+    origin: "*",
 }))
 
 
@@ -14,8 +14,8 @@ const port = process.env.PORT || 8383
 // app.use(express.static('../Frontend'))
 app.use(express.json())
 
-var _code,_lang,_input,_output
-app.get('/',(req,res)=>{
+var _code, _lang, _input, _output
+app.get('/', (req, res) => {
     console.log("this is get normal")
     res.status(200).send("Hello world")
 })
@@ -41,26 +41,26 @@ app.get('/getOutput', (req, res) => {
     axios(config)
         .then(function (res) {
             // console.log(res.data)
-           _output = JSON.stringify(res.data)
-           res.data={}
-           return _output
+            _output = JSON.stringify(res.data)
+            res.data = {}
+            return _output
         })
-        .then(()=>{
+        .then(() => {
 
-            console.log("output : ",_output)
-            if((JSON.parse(_output)).status==200){
-                if((JSON.parse(_output)).error==""){
-                    res.status(200).send({ getOutput: (JSON.parse(_output)).output})
+            console.log("output : ", _output)
+            if ((JSON.parse(_output)).status == 200) {
+                if ((JSON.parse(_output)).error == "") {
+                    res.status(200).send({ getOutput: (JSON.parse(_output)).output })
                 }
                 else {
-                    res.status(200).send({ getOutput: (JSON.parse(_output)).error})
+                    res.status(200).send({ getOutput: (JSON.parse(_output)).error })
                 }
             }
             else {
-                res.send({ getOutput: (JSON.parse(_output)).error})
+                res.send({ getOutput: (JSON.parse(_output)).error })
             }
         })
-        .catch((error)=> {
+        .catch((error) => {
             console.log(error);
             // console.log("punki")
         });
@@ -72,29 +72,29 @@ app.post('/', (req, res) => {
     const { code } = req.body
     const { language } = req.body
     const { input } = req.body
-    
-    // console.log("check : ",req.body)
-    
-    _code=code
-    _lang=language
-    _input=input
 
-    if(_code!=undefined && _lang!=undefined && _input!=undefined) {
+    // console.log("check : ",req.body)
+
+    _code = code
+    _lang = language
+    _input = input
+
+    if (_code != undefined && _lang != undefined && _input != undefined) {
         let dmd = JSON.stringify({
-            a:_code,
-            b:_lang,
-            c:_input
+            a: _code,
+            b: _lang,
+            c: _input
         })
         // console.log("post : ",dmd)
         // res.status(200).send(dmd)
     }
-    else{
+    else {
         console.log("error")
         // res.send("error")
     }
 
     // Post The data
-    
+
     var postData = qs.stringify({
         code: _code,
         language: _lang,
@@ -113,26 +113,26 @@ app.post('/', (req, res) => {
     axios(config)
         .then(function (res) {
             // console.log(res.data)
-           _output = JSON.stringify(res.data)
-           res.data={}
-           return _output
+            _output = JSON.stringify(res.data)
+            res.data = {}
+            return _output
         })
-        .then(()=>{
+        .then(() => {
 
             // console.log("output : ",_output)
-            if((JSON.parse(_output)).status==200){
-                if((JSON.parse(_output)).error==""){
-                    res.status(200).send({ getOutput: (JSON.parse(_output)).output})
+            if ((JSON.parse(_output)).status == 200) {
+                if ((JSON.parse(_output)).error == "") {
+                    res.status(200).send({ getOutput: (JSON.parse(_output)).output })
                 }
                 else {
-                    res.status(200).send({ getOutput: (JSON.parse(_output)).error})
+                    res.status(200).send({ getOutput: (JSON.parse(_output)).error })
                 }
             }
             else {
-                res.send({ getOutput: (JSON.parse(_output)).error})
+                res.send({ getOutput: (JSON.parse(_output)).error })
             }
         })
-        .catch((error)=> {
+        .catch((error) => {
             console.log(error);
         });
 })
